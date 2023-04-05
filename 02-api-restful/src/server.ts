b01ec9380dc30f3c7d8c67b2a352fcab1f906f7e
Enilton Rodrigues de Andrade <erodri@Eniltons-MacBook-Pro-2.local>
2023-04-05 14:09:47 -0500

    [02] Database setup

--- 02-api-restful/src/server.ts
+++ 02-api-restful/src/server.ts
@@ -1,14 +1,29 @@
 import fastify from 'fastify'
+import { knex } from './database'
+import crypto from 'node:crypto'
+import { env } from './env'
 
 const app = fastify()
 
-app.get('/hello', () => {
-  return 'Hello World'
+app.get('/hello', async () => {
+  const transaction = await knex('transactions')
+    .insert({
+      id: crypto.randomUUID(),
+      title: 'Test Transaction',
+      amount: 1000,
+    })
+    .returning('*')
+
+  // const transaction = await knex('transactions')
+  //   .where('amount', 1000)
+  //   .select('*')
+
+  return transaction
 })
 
 app
   .listen({
-    port: 3333,
+    port: env.PORT,
   })
   .then(() => {
     console.log('HTTP Server Running')
