4f752da90ed646ce2e5612f9793ae271a0876470
Enilton Rodrigues de Andrade <erodri@Eniltons-MacBook-Pro-2.local>
2023-04-11 11:15:27 -0500

    [03] Applied SOLID design pattern

---
+++ 03-api-solid/src/repositories/users-repository.ts
@@ -0,0 +1,5 @@
+import { Prisma, User } from '@prisma/client'
+export interface UserRepository {
+  findByEmail(email: string): Promise<User | null>
+  create(data: Prisma.UserCreateInput): Promise<User>
+}
---
+++ 03-api-solid/src/use-cases/register.ts
@@ -0,0 +1,29 @@
+import { UserAlreadyExistsError } from '@/use-cases/errors/user-already-exists-error'
+import { UserRepository } from '@/repositories/users-repository'
+import { hash } from 'bcryptjs'
+
+interface RegisterUseCaseRequest {
+  name: string
+  email: string
+  password: string
+}
+
+export class RegisterUseCase {
+  constructor(private userRepository: UserRepository) {}
+
+  async execute({ name, email, password }: RegisterUseCaseRequest) {
+    const password_hash = await hash(password, 6)
+
+    const userWithSameEmail = await this.userRepository.findByEmail(email)
+
+    if (userWithSameEmail) {
+      throw new UserAlreadyExistsError()
+    }
+
+    await this.userRepository.create({
+      name,
+      email,
+      password_hash,
+    })
+  }
+}
