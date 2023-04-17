5ab6f489a79ee49a799551eb274fd055df6650a8
Enilton Rodrigues de Andrade <erodri@Eniltons-MacBook-Pro-2.local>
2023-04-17 14:02:57 -0500

    [04] - Created Check-In Controllers

--- 03-api-solid/src/use-cases/factories/make-get-user-metrics-use-case.ts
+++ 03-api-solid/src/use-cases/factories/make-get-user-metrics-use-case.ts
@@ -1,7 +1,7 @@
 import { GetUserMetricsUseCase } from '../get-user-metrics'
 import { PrismaCheckInRepository } from '@/repositories/prisma/prisma-check-ins-repository'
 
-export function makeGetUserUserMetricsUseCase() {
+export function makeGetUserMetricsUseCase() {
   const repository = new PrismaCheckInRepository()
   const useCase = new GetUserMetricsUseCase(repository)
 
