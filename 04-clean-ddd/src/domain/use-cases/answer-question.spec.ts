129512356b1d0c71046a97849dc94f5a2336ced2
Enilton Rodrigues de Andrade <erodri@tm0701908.gsm1900.org>
2023-05-01 09:57:20 -0500

    04 - Clean DDD

---
+++ 04-clean-ddd/src/domain/use-cases/answer-question.spec.ts
@@ -0,0 +1,21 @@
+import { AnswerQuestionUseCase } from './answer-question'
+import { AnswerRepository } from '../repositories/asnwers-repository'
+import { Answer } from '../entities/answer'
+
+const fakeAnswersRespository: AnswerRepository = {
+    create: async (answer: Answer) => {
+        return
+    }
+}
+
+test('create an answer', async ()=>{
+    const answerQuestion = new AnswerQuestionUseCase(fakeAnswersRespository)
+
+    const answer =  await answerQuestion.execute({
+        content: 'Nova Resposta',
+        instructorId: '1',
+        questionId: '1'
+    })
+
+    expect(answer.content).toEqual('Nova Resposta')
+})
